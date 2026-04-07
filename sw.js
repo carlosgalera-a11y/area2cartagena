09d6b107e125ebc9d9427c54072d90125d0c1516
Carlos Galera <carlosgalera2roman@gmail.com>
2026-04-07 00:03:23 +0000

    FORCE DOCS v2: click-based DOM renderer, bypasses all caching

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker - Área II Cartagena PWA v36 - network-first + precache sections
-const CACHE_NAME = 'area2-v61';
+const CACHE_NAME = 'area2-v62';
 
 const PRECACHE = [
   '/Cartagenaeste/',
